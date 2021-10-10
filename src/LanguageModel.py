7d7e2b1ae6c5a10456ff73c96d293799b2142c54
Sara Budts <47114253+srbdts@users.noreply.github.com>
2021-10-10 15:05:48 +0200

    Update LanguageModel.py
    Added docstrings for clarity

--- src/LanguageModel.py
+++ src/LanguageModel.py
@@ -9,17 +9,17 @@ class LanguageModel:
         self.load()
 
     def load(self):
+        """ load language model from log probability files """
         prob_data = open(self.probsfile,"r",encoding="utf-8")
         self.probs = {trigram.split("\t")[0]:float(trigram.split("\t")[2]) for trigram in prob_data.readlines()}
 
     def get_local_prob(self,trigram):
+        """ return the probability of given trigram in current language """
         local_prob = 0
         if trigram in self.probs:
             return self.probs[trigram]
         else:
             return self.probs["UNKNOWN"]
 
-    def calculate_probs(self,word):
 
-        return
 
